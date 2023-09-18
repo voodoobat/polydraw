1d737076396ecae703851c0f12144e60eb5f4d41
Vova Rodin <mynameisvova@yahoo.com>
2023-09-18 20:29:27 +0300

    fix code style

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,13 +3,13 @@ const path = require('path')
 module.exports = {
     entry: './src/index.ts',
     module: {
-    rules: [
-        {
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        },
-    ],
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
     },
     resolve: {
         extensions: [ '.ts', '.js' ],
