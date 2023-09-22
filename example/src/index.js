0b54485a8f591f69dfccbe1b3efac3f48b647eaa
Vova Rodin <mynameisvova@yahoo.com>
2023-09-22 10:31:10 +0300

    window example

--- example/src/index.js
+++ example/src/index.js
@@ -1,51 +1,55 @@
 import { polydraw } from 'polydraw'
 
-const draw = await polydraw('#image-2', '/public/img.jpg', {})
-polydraw('#image-1', '/public/img.jpg', {
-    events: {
-        onPolygonCreate: (polygon) => {
-            draw.placePolygon(polygon)
-        },
-        onPolygonChange: (polygon) => {
-            draw.placePolygon(polygon)
-        },
-        onSceneChange: (data) => {
-            console.log(data)
-        },
-    },
-})
+window.polydraw = polydraw
 
-polydraw('#image-3', '/public/img.jpg', {
-    elements: {
-        point: {
-            size: 15,
-            color: 'black',
-            opacity: 1,
-        },
-        circuit: {
-            size: 2,
-            color: 'black',
-            opacity: 0.25,
-        },
-        guide: {
-            size: 3,
-            color: 'black',
-            opacity: 0.5,
-        },
-        polygon: {
-            color: 'black',
-            opacity: 0.15,
-            point: {
-                size: 15,
-                color: 'black',
-                opacity: 0.5,
-            },
-        },
-        menu: {
-            style: {
-                fontSize: '1.2em',
-                background: 'black',
-            },
-        },
-    },
-})
+// const draw = await polydraw('#image-2', '/public/img.jpg', {})
+// polydraw('#image-1', '/public/img.jpg', {
+//     events: {
+//         onPolygonCreate: (polygon) => {
+//             draw.placePolygon(polygon)
+//         },
+//         onPolygonChange: (polygon) => {
+//             draw.placePolygon(polygon)
+//         },
+//         onSceneChange: (data) => {
+//             console.log(data)
+//         },
+//     },
+// })
+//
+// polydraw('#image-3', '/public/img.jpg', {
+//     elements: {
+//         point: {
+//             size: 15,
+//             color: 'black',
+//             opacity: 1,
+//         },
+//         circuit: {
+//             size: 2,
+//             color: 'black',
+//             opacity: 0.25,
+//         },
+//         guide: {
+//             size: 3,
+//             color: 'black',
+//             opacity: 0.5,
+//         },
+//         polygon: {
+//             color: 'black',
+//             opacity: 0.15,
+//             point: {
+//                 size: 15,
+//                 color: 'black',
+//                 opacity: 0.5,
+//             },
+//         },
+//         menu: {
+//             style: {
+//                 fontSize: '1.2em',
+//                 background: 'black',
+//             },
+//         },
+//     },
+// })
+//
+// window.polydraw = polydraw
